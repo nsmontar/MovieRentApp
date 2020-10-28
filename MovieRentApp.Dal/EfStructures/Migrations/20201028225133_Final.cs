214b02d87be32115ed565b4acd34b09ea48a329a
Nenad <nsmontar@gmail.com>
2020-10-28 23:53:35 +0100

    Final migration and completed database

---
+++ MovieRentApp.Dal/EfStructures/Migrations/20201028225133_Final.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieRentApp.Dal.EfStructures.Migrations
+{
+    public partial class Final : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameIndex(
+                name: "IX_Customers",
+                schema: "MovieRent",
+                table: "Users",
+                newName: "IX_Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                schema: "MovieRent",
+                table: "Movies",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RentalDate",
+                schema: "MovieRent",
+                table: "Movies",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReturnDate",
+                schema: "MovieRent",
+                table: "Movies",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                schema: "MovieRent",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "RentalDate",
+                schema: "MovieRent",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "ReturnDate",
+                schema: "MovieRent",
+                table: "Movies");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users",
+                schema: "MovieRent",
+                table: "Users",
+                newName: "IX_Customers");
+        }
+    }
+}
--- MovieRentApp.Models/ViewModels/RentedMovies.cs
+++ MovieRentApp.Models/ViewModels/RentedMovies.cs
@@ -1,10 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using MovieRentApp.Models.Entities;
 
 namespace MovieRentApp.Models.ViewModels
 {
+    [Table("RentedMoviesView", Schema = "MovieRent")]
     public class RentedMovies : Movie
     {
-        
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Date Shipped")]
+        public DateTime RentalDate { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Date Shipped")]
+        public DateTime ReturnDate { get; set; }
     }
 }
